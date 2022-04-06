b137af3fe6d3e5be3375c8988381ce5fc6b7b282
zsantiago8712 <zsantiago8788@icloud.com>
2022-04-06 02:05:54 -0500

    Correciones

--- src/com/sz/gi/Main.java
+++ src/com/sz/gi/Main.java
@@ -3,6 +3,9 @@ package com.sz.gi;
 public class Main {
 
     public static void main(String[] args){
+        Window app = new Window("app", 90, 90);
+
+        app.setVisible(true);
         DataBase db = new DataBase();
     }
 
--- src/com/sz/gi/MainTest.java
+++
@@ -1,2 +0,0 @@
-package com.sz.gi;public class MainTest {
-}
